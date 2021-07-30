e7977c2c5b53cccb4b82ce2df4926eb98f2a193e
thomas <thomasmonta98@gmail.com>
2021-07-30 12:22:49 +1000

    done with assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  console.log(actors)
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor => (
+        <div key={actor.name}>
+          <h2>{actor.name}</h2>
+          <ul>
+            {actor.movies.map(movie => <li key={movie}>{movie}</li>)}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,16 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => (
+          <div key={movie.title}>
+            <h2>{movie.title}</h2>
+            <h4>time: {movie.time}</h4>
+            <ul>
+              {movie.genres.map(genre => <li key={genre}>{genre}</li>)}
+            </ul>
+          </div>
+        ))}
     </div>
   );
 };
